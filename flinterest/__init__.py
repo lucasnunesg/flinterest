b46e392734f64924f671bbaf2d78ce300bd00dc9
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2023-12-09 18:19:18 -0300

    edited `requirements.txt`

--- flinterest/__init__.py
+++ flinterest/__init__.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///community.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "a453c67ec6c1afa3d57e97166987c944"
 app.config["UPLOAD_FOLDER"] = "static/post_pictures"
 
